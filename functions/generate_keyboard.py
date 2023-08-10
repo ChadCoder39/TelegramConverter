319f05f471fe258195b8e84e8c7676bee8e7dba8
bohdan123__ <krolbohdan1@gmail.com>
2023-08-10 13:59:55 +0200

    file sorting added

--- functions/generate_keyboard.py
+++ functions/generate_keyboard.py
@@ -3,11 +3,20 @@ from telebot import types
 
 def generate_keyboard(extension):
     keyboard = types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
-    keyboard.add(types.KeyboardButton('Convert to PDF'))
-    keyboard.add(types.KeyboardButton('Convert to DOCX'))
-    keyboard.add(types.KeyboardButton('Convert to TXT'))
-    keyboard.add(types.KeyboardButton('Convert to JPG'))
-    keyboard.add(types.KeyboardButton('Convert to PNG'))
-    keyboard.add(types.KeyboardButton('Convert to MP3'))
+    if extension == 'txt':
+        keyboard.add(types.KeyboardButton('Convert to PDF'))
+        keyboard.add(types.KeyboardButton('Convert to DOCX'))
+    elif extension == 'pdf':
+        keyboard.add(types.KeyboardButton('Convert to TXT'))
+        keyboard.add(types.KeyboardButton('Convert to DOCX'))
+    elif extension == 'docx':
+        keyboard.add(types.KeyboardButton('Convert to PDF'))
+        keyboard.add(types.KeyboardButton('Convert to TXT'))
+    elif extension == 'jpg':
+        keyboard.add(types.KeyboardButton('Convert to PNG'))
+    elif extension == 'png':
+        keyboard.add(types.KeyboardButton('Convert to JPG'))
+    elif extension == 'mp4':
+        keyboard.add(types.KeyboardButton('Convert to MP3'))
 
     return keyboard
